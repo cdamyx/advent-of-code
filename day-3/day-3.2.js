fa05dd4ba8a179e4c03a2459af15b050428849cd
cdamyx <chris@amyx.engineering>
2023-12-06 00:56:24 -0600

    Complete day-3.2

---
+++ day-3/day-3.2.js
@@ -0,0 +1,92 @@
+const helpers = require('../helpers')
+let data = helpers.data('./input')
+let isAsterisk = (c) => {return c.match(/\*/)}
+let currentNumber = ''
+let total = 0
+let gears = []
+let gearsTotal = 0
+
+const lookLeft = (row, prevIndex) => {
+    if(Number(row[prevIndex]) || Number(row[prevIndex]) === 0){
+        let counter = prevIndex
+        currentNumber = ''
+        while(Number(row[counter]) || Number(row[counter]) === 0){
+            currentNumber = row[counter] + currentNumber
+            counter--
+        }
+        total += Number(currentNumber)
+        // console.log(currentNumber + 'left')
+        // console.log(total + 'total');
+        gears.push(currentNumber)
+    }
+}
+
+const lookRight = (row, nextIndex) => {
+    if(Number(row[nextIndex]) || Number(row[nextIndex]) === 0){
+        let counter = nextIndex
+        currentNumber = ''
+        while(Number(row[counter]) || Number(row[counter]) === 0){
+            currentNumber = currentNumber + row[counter]
+            counter++
+        }
+        total += Number(currentNumber)
+        // console.log(currentNumber + 'right')
+        // console.log(total + 'total');
+        gears.push(currentNumber)
+    }
+}
+
+const lookMiddle = (row, currIndex) => {
+    if(Number(row[currIndex]) || Number(row[currIndex]) === 0){
+        currentNumber = ''
+        let rightCounter = currIndex + 1
+        let counter = currIndex
+        currentNumber = ''
+        while(Number(row[counter]) || Number(row[counter]) === 0){
+            currentNumber = row[counter] + currentNumber
+            counter--
+        }
+        
+        while(Number(row[rightCounter]) || Number(row[rightCounter]) === 0){
+            currentNumber = currentNumber + row[rightCounter]
+            rightCounter++
+        }
+        total += Number(currentNumber)
+        // console.log(currentNumber + 'middle');
+        // console.log(total + 'total');
+        gears.push(currentNumber)
+    }
+}
+
+for(let i = 1; i < data.length - 1; i++){
+    let lastRow = data[i - 1]
+    let currentRow = data[i]
+    let nextRow = data[i + 1]
+    
+    for(let j = 0; j < currentRow.length; j++){
+        if(isAsterisk(currentRow[j])){
+            lookLeft(currentRow, j - 1)
+            lookRight(currentRow, j + 1)
+            if(Number(lastRow[j]) || Number(lastRow[j]) === 0){
+                lookMiddle(lastRow, j)
+            } else {
+                lookLeft(lastRow, j - 1)
+                lookRight(lastRow, j + 1)
+            }
+            if(Number(nextRow[j]) || Number(nextRow[j]) === 0){
+                lookMiddle(nextRow, j)
+            } else {
+                lookLeft(nextRow, j - 1)
+                lookRight(nextRow, j + 1)
+            }
+        }
+        if(gears.length === 2){
+            gearsTotal += (gears[0] * gears[1])
+            gears = []
+        } else {
+            gears = []
+        }
+    }
+}
+
+console.log(gearsTotal)
