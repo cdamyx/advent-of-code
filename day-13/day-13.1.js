34ed78d084e93787724102db7daf6a235d08d483
cdamyx <chris@amyx.engineering>
2023-12-18 12:27:57 -0600

    Initial commit day 13

---
+++ day-13/day-13.1.js
@@ -0,0 +1,9 @@
+const helpers = require('../helpers')
+let data = helpers.data('./input')
+let currentPatternStart = 0
+let currentPatternEnd = 0
+let nextPatternStart = 0
+
+
+
+console.log(data[18] === data[20] ? true : false);
